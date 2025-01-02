d2cf63b4e1fdcc6abcd4c417ab67035e2bb8f34a
ItachiPrabin <itachiprabin@gmail.com>
2025-01-02 22:41:58 +0545

    updated simple interst

--- lib/cubit/simple_interest_cubit.dart
+++ lib/cubit/simple_interest_cubit.dart
@@ -1,3 +1,14 @@
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// class SimpleInterestCubit extends Cubit<double> {
+//   SimpleInterestCubit() : super(0.0);
+
+//   void calculate(double principal, double rate, double time) {
+//     double simpleInterest = (principal * rate * time) / 100;
+//     emit(simpleInterest);
+//   }
+// }
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimpleInterestCubit extends Cubit<double> {
